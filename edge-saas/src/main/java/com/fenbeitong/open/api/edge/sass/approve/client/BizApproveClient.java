4477f7a238353860fe3a2902187fab18e093131b
ivan <ivandjp@163.com>
2018-11-20 10:15:54 +0800

    Add ignor

--- edge-saas/src/main/java/com/fenbeitong/open/api/edge/sass/approve/client/BizApproveClient.java
+++ edge-saas/src/main/java/com/fenbeitong/open/api/edge/sass/approve/client/BizApproveClient.java
@@ -1,5 +1,12 @@
 package com.fenbeitong.open.api.edge.sass.approve.client;
 
+import com.fenbeitong.open.api.edge.sass.approve.entity.ApproveEntity;
+import com.fenbeitong.open.api.edge.sass.approve.vo.ApproveParam;
+import com.fenbeitong.open.api.support.commons.model.entity.backend.BizCommonEntity;
+import com.fenbeitong.open.api.support.commons.model.vo.backend.BizCommonParams;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 /**
  * BizApproveClient
  * <p> TODO
@@ -8,5 +15,8 @@ package com.fenbeitong.open.api.edge.sass.approve.client;
  * @version 1.0
  * Created by ivan on 18-11-19 - 下午8:14.
  **/
-public class BizApproveClient {
+public interface BizApproveClient {
+
+    @PostMapping("/create")
+    public BizCommonEntity<ApproveEntity> createApprove(@RequestBody BizCommonParams<ApproveParam> bizCommonParams);
 }
