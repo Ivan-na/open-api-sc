bb86587b79597d9d8f87654f7188d2ee86d819ba
ivan <ivandjp@gmail.com>
2018-12-06 21:07:00 +0800

    modify response/exception

--- support-web/src/main/java/model/dto/response/ApiResponse.java
+++ support-web/src/main/java/model/dto/response/ApiResponse.java
@@ -12,27 +12,27 @@ import com.fenbeitong.open.api.service.jwt.support.commons.constants.ResponseCod
  */
 public class ApiResponse<T> implements java.io.Serializable {
 
-    private static final long serialVersionUID = -467317048623810531L;
+  private static final long serialVersionUID = -467317048623810531L;
 
-    /**
-     * @return model.dto.response.ApiResponse<java.lang.Void>
-     * @author Created by ivan on 下午8:42 18-12-6.
-     * <p>空数据返回，如DELETE
-     */
-    public static ApiResponse<Void> empty() {
-        return SuccessResponse.<Void>builder()
-                .responseCode(ResponseCode.SUCCESS_CODE.getCode())
-                .build();
-    }
+  /**
+   * @return model.dto.response.ApiResponse<java.lang.Void>
+   * @author Created by ivan on 下午8:42 18-12-6.
+   *     <p>空数据返回，如DELETE
+   */
+  public static ApiResponse<Void> empty() {
+    return SuccessResponse.<Void>builder()
+        .responseCode(ResponseCode.SUCCESS_CODE.getCode())
+        .build();
+  }
 
-    public static <T> ApiResponse<T> success(T responseData) {
-        return SuccessResponse.<T>builder()
-                .responseCode(ResponseCode.SUCCESS_CODE.getCode())
-                .responseData(responseData)
-                .build();
-    }
+  public static <T> ApiResponse<T> success(T responseData) {
+    return SuccessResponse.<T>builder()
+        .responseCode(ResponseCode.SUCCESS_CODE.getCode())
+        .responseData(responseData)
+        .build();
+  }
 
-//  public static <T> ApiResponse<T> failure(){
-//
-//  }
+  //  public static <T> ApiResponse<T> failure(){
+  //
+  //  }
 }
